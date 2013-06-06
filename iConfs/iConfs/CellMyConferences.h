9d6804e0fd9764cda357dfb6fa3c99a9fd466234
Eduardo Joel Pereira Beja Martins <bejamartins@gmail.com>
2013-06-06 12:21:47 +0100

    coisas da ana -p1

---
+++ iConfs/iConfs/CellMyConferences.h
@@ -0,0 +1,17 @@
+//
+//  CellMyConferences.h
+//  iConfs
+//
+//  Created by Ana T on 05/06/13.
+//  Copyright (c) 2013 Eduardo Joel Pereira Beja Martins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellMyConferences : UICollectionViewCell
+//@property (strong, nonatomic) IBOutlet UIImageView *conferenceLogo;
+//@property (strong, nonatomic) IBOutlet UILabel *conferenceLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *conferenceLogo;
+@property (strong, nonatomic) IBOutlet UILabel *conferenceLabel;
+
+@end
---
+++ iConfs/iConfs/CellMyConferences.m
@@ -0,0 +1,31 @@
+//
+//  CellMyConferences.m
+//  iConfs
+//
+//  Created by Ana T on 05/06/13.
+//  Copyright (c) 2013 Eduardo Joel Pereira Beja Martins. All rights reserved.
+//
+
+#import "CellMyConferences.h"
+
+@implementation CellMyConferences
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
