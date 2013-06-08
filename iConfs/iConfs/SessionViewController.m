a43c884baca035062ad71140580e57441dc635f5
Ana-T <anat_pereira@hotmail.com>
2013-06-09 00:30:19 +0100

    +coisas

---
+++ iConfs/iConfs/SessionViewController.m
@@ -0,0 +1,38 @@
+//
+//  SessionViewController.m
+//  iConfs
+//
+//  Created by Ana T on 09/06/13.
+//  Copyright (c) 2013 Eduardo Joel Pereira Beja Martins. All rights reserved.
+//
+
+#import "SessionViewController.h"
+
+@interface SessionViewController ()
+
+@end
+
+@implementation SessionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
