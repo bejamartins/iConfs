62b2ebd21b42c58d22efa0ceaa49639c84c4c8bf
Ana-T <anat_pereira@hotmail.com>
2013-06-11 00:21:58 +0100

    sasa

--- iConfs/iConfs/BluePrintsViewController.m
+++ iConfs/iConfs/BluePrintsViewController.m
@@ -7,25 +7,20 @@
 //
 
 #import "BluePrintsViewController.h"
-
+#import "FloorCell.h"
 @interface BluePrintsViewController ()
 
 @end
 
 @implementation BluePrintsViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+   [ self.collection setDataSource:self];
+   [ self.collection setDelegate:self];
 	// Do any additional setup after loading the view.
 }
 
@@ -33,6 +28,26 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Collection datasource and delegate
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return 1;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *CellIdentifier=@"floor";
+    FloorCell *cell= [collectionView dequeueReusableCellWithReuseIdentifier:CellIdentifier forIndexPath:indexPath];
+    [[cell picture]setImage:[UIImage imageNamed:@"speaker.jpg"]];
+    
+    [[cell floorName]setText:@"floor"];
+    
+    return cell;
 }
 
 @end
