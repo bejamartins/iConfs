e2325d4d10101107bdb9a4cb955e0b636bac155d
Jareth <jareth@Jareths-Mac-Pro.local>
2013-06-20 16:24:41 -0700

    Add Sessions

---
+++ iConfs/iConfs/AddSessionViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddSessionViewController.m
+//  iConfs
+//
+//  Created by Jareth on 6/20/13.
+//  Copyright (c) 2013 Eduardo Joel Pereira Beja Martins. All rights reserved.
+//
+
+#import "AddSessionViewController.h"
+
+@interface AddSessionViewController ()
+
+@end
+
+@implementation AddSessionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
